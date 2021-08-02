3918169c26bfaeb6e7aea7da484754561d914887
Christopher Kirkley <ckirkley@gmail.com>
2021-08-02 10:55:24 -0700

    Merged feature branch

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var round = 0;
+
 var score = {
     'player' : 0,
     'computer' : 0,
@@ -22,7 +24,36 @@ const computerPlay = () => {
 
 
 const renderResults = (str) => {
-    console.log(str);
+    results = document.querySelector('#results');
+    results.innerText = `Round ${round + 1}: ${str}`;
+
+    const playerValue = document.querySelector('#playerValue');
+    const computerValue = document.querySelector('#computerValue');
+    const tieValue = document.querySelector('#tieValue');
+
+    playerValue.innerText = score['player'];
+    computerValue.innerText = score['computer'];
+    tieValue.innerText = score['tie'];
+
+}
+
+const renderWinner = () => {
+    const playerTotal = score['player'];
+    const computerTotal = score['computer'];
+
+    winner = document.querySelector('#winner');
+
+    if (playerTotal > computerTotal) {
+        winner.innerText =  'You have defeated Skynet.';
+    }
+
+    else if (computerTotal > playerTotal) {
+        winner.innerText =  'Machine wins. Dark days for humankind.';
+    } else {
+        winner.innerText =  'A TIE! Meet again on the fields of Vahalla.';
+    }
+
+
 }
 
 
@@ -30,50 +61,125 @@ const playRound = (playerSelection, computerSelection) => {
 	var playerSelection = playerSelection.toUpperCase();
 	var computerSelection = computerSelection.toUpperCase();
 
-    // if tie
-	if (playerSelection == computerSelection) {
-		renderResults(`You tie! ${toTitleCase(playerSelection)} is the same as ${toTitleCase(computerSelection)}`)
-        score['tie'] ++
-	}
+    if (round < 5) {
 
-    // if computer wins
-	if (
-        (computerSelection == 'ROCK' && playerSelection == 'SCISSORS') ||
-        (computerSelection == 'SCISSORS' && playerSelection == 'PAPER') ||
-        (computerSelection == 'PAPER' && playerSelection == 'ROCK'))
-    {
-		renderResults(`You lose! ${toTitleCase(computerSelection)} beats ${toTitleCase(playerSelection)}`)
-        score['computer'] ++
-    }   
+        // if tie
+        if (playerSelection == computerSelection) {
+            score['tie'] ++
+            renderResults(`You tie! ${toTitleCase(playerSelection)} is the same as ${toTitleCase(computerSelection)}`)
+        }
 
-    // if player wins
-    if (
-        (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') ||
-        (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') ||
-        (playerSelection == 'PAPER' && computerSelection == 'ROCK'))
-    {
-		renderResults(`You win! ${toTitleCase(playerSelection)} beats ${toTitleCase(computerSelection)}`)
-        score['player'] ++
-    }   
+        // if computer wins
+        if (
+            (computerSelection == 'ROCK' && playerSelection == 'SCISSORS') ||
+            (computerSelection == 'SCISSORS' && playerSelection == 'PAPER') ||
+            (computerSelection == 'PAPER' && playerSelection == 'ROCK'))
+        {
+            score['computer'] ++
+            renderResults(`You lose! ${toTitleCase(computerSelection)} beats ${toTitleCase(playerSelection)}`)
+        }   
+
+        // if player wins
+        if (
+            (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') ||
+            (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') ||
+            (playerSelection == 'PAPER' && computerSelection == 'ROCK'))
+        {
+            score['player'] ++
+            renderResults(`You win! ${toTitleCase(playerSelection)} beats ${toTitleCase(computerSelection)}`)
+        }   
+
+        round ++;
+
+    }
+
+    if (round == 5) {
+        renderWinner()
+    }
+
 }
 
+const renderBoard = () => {
+    // cache DOM
+    const main = document.querySelector('#main');
+
+    var div = document.createElement('div');
+    div.classList.add('selection');
+    const rockBtn = document.createElement('button');
+    rockBtn.innerText = 'Rock';
+    rockBtn.value = 'rock';
+    div.appendChild(rockBtn);
+    main.appendChild(div);
+
+    var div = document.createElement('div');
+    div.classList.add('selection');
+    const paperBtn = document.createElement('button');
+    paperBtn.innerText = 'Paper';
+    paperBtn.value = 'paper';
+    div.appendChild(paperBtn);
+    main.appendChild(div);
+
+    var div = document.createElement('div');
+    div.classList.add('selection');
+    const scissorsBtn = document.createElement('button');
+    scissorsBtn.innerText = 'Scissors';
+    scissorsBtn.value = 'scissors';
+    div.appendChild(scissorsBtn);
+    main.appendChild(div);
+
+    var div = document.createElement('div');
+    div.id = 'results';
+    main.appendChild(div);
+
+    var div = document.createElement('div');
+    div.classList.add('selection');
+    div.id = 'score';
+    const playerScore = document.createElement('div')
+    playerScore.id = 'playerScore';
+    playerScore.append(document.createElement('p').innerHTML = 'Player: ')
+    const playerValue = document.createElement('span');
+    playerValue.id = 'playerValue';
+    playerScore.append(playerValue);
+    div.append(playerScore)
+
+    const computerScore = document.createElement('div')
+    computerScore.id = 'computerScore';
+    computerScore.append(document.createElement('p').innerHTML = 'Computer: ');
+    const computerValue = document.createElement('span');
+    computerValue.id = 'computerValue';
+    computerScore.append(computerValue);
+    div.append(computerScore);
+
+    const tieScore = document.createElement('div')
+    tieScore.append(document.createElement('p').innerHTML = 'Tie: ')
+    const tieValue = document.createElement('span');
+    tieValue.id = 'tieValue';
+    tieScore.append(tieValue);
+    tieScore.id = 'tieScore';
+    div.append(tieScore)
+    main.appendChild(div);
+
+    var div = document.createElement('div');
+    div.id = 'winner';
+    main.appendChild(div);
+
+}
 
 const game = () => {
 	var playerSelection = '';
 
-	while (!choices.includes(playerSelection)) {
-		var playerSelection = prompt("What's your choice?");
-        if (!playerSelection) {
-            console.log('Type something for the love of god!');
-        } else {
-            playerSelection = playerSelection.toUpperCase();
-        }
-	}
+    renderBoard();
 
-    for (let i = 0; i < 5; i++) {
-        var res = playRound(playerSelection, computerPlay());
-    }
-    console.log(score);
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach((btn) => btn.addEventListener('click', (e) => playRound(e.currentTarget.value, computerPlay())))
+
+
+
 }
 
+
 game();
+
+
+
